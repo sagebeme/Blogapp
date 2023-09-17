52685c429e6d3bb07074cf53028a01b9aa087a13
sagebeme <eudicas5k@gmail.com>
2023-09-17 16:23:22 +0300

    file structure

--- The_app/api/v1/views/__init__.py
+++ The_app/api/v1/views/__init__.py
@@ -5,6 +5,6 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__)
 
-from The_app.api.v1.views.post_views import *
-from The_app.api.v1.views.status import *
+from api.v1.views.post_views import *
+from api.v1.views.status import *
 
--- The_app/api/v1/views/status.py
+++ The_app/api/v1/views/status.py
@@ -1,4 +1,4 @@
-from The_app.api.v1.views import app_views
+from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route('/status', methods=['GET'])
